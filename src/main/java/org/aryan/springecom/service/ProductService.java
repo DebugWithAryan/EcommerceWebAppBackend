af0a064b74a7c60bc1eba246a5edebd21af2d19d
DebugWithAryan <aryanjaiswal123123@gmail.com>
2025-08-20 21:34:28 +0530

    Added Search and Images functionality

--- src/main/java/org/aryan/springecom/service/ProductService.java
+++ src/main/java/org/aryan/springecom/service/ProductService.java
@@ -4,7 +4,9 @@ import org.aryan.springecom.model.Product;
 import org.aryan.springecom.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -12,7 +14,30 @@ public class ProductService {
 
     @Autowired
     private ProductRepo productRepo;
-    public List<Product> getAllProducts(){
+
+    public List<Product> getAllProducts() {
         return productRepo.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return productRepo.findById(id).orElse(new Product(-1));
+    }
+
+    public Product addOrUpdateProduct(Product product, MultipartFile image) throws IOException {
+        product.setImageName(image.getOriginalFilename());
+        product.setImageType(image.getContentType());
+        product.setImageData(image.getBytes());
+
+        return productRepo.save(product);
+    }
+
+
+    public void deleteProduct(int id) {
+        productRepo.deleteById(id);
+    }
+
+
+    public List<Product> searchProducts(String keyword) {
+        return productRepo.searchProducts(keyword);
     }
 }
